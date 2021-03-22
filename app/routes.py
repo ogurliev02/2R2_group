aa2e0a488dd1bf6473fd302dce3ba6d8888e1774
Гамид Огурлиев <ogurliev02@MBP-Gamid.mshome.net>
2021-03-22 14:05:31 +0300

    add 22.03

--- app/routes.py
+++ app/routes.py
@@ -72,3 +72,33 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        return 'Такого пользователя нет! На него нельзя подписаться!'
+    
+    if user == current_user:
+        return 'К сожалению, Вы не сможете подписаться сами на себя...'
+
+    current_user.follow(user)
+    db.session.commit()
+    return render_template(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        return 'Такого пользователя нет! От него нельзя отписаться!'
+
+    if user == current_user:
+        return 'К сожалению, Вы не сможете отписаться от самого себя...'
+
+    current_user.unfollow(user)
+    db.session.commit()
+    return render_template(url_for('user', username=username))
