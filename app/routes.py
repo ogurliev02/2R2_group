970677813739d2bcff340b17a4389c44b813b835
Гамид Огурлиев <ogurliev02@MacBook-Pro-Gamid.local>
2021-03-30 22:22:36 +0300

    add 30.03

--- app/routes.py
+++ app/routes.py
@@ -22,22 +22,38 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
 
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+            page, app.config['POSTS_PER_PAGE'], False)
+
+    
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home Page', form=form, posts=posts.items, 
+                                            next_url=next_url, prev_url=prev_url)
 
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+                page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items,
+                                next_url=next_url, prev_url=prev_url)
 
 @app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    user_posts = Post.query.filter_by(author=user)
-    return render_template('user.html', user=user, posts=user_posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+                        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
